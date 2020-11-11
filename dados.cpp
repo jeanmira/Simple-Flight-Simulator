0464eee9700e96ea7d5c407c956e2c96096ec175
Jean Marcelo MIra Junior <jeandemira@gmail.com>
2020-11-11 11:10:40 -0300

    Identacao automatica VSCODE

--- dados.cpp
+++ dados.cpp
@@ -6,45 +6,48 @@
 namespace SimuladorDeVooSimples
 {
 
-// Set o pitch,roll e o yaw de acordo com as posições dos dados
-void Dados::setGiroscopio(float pitch, float roll, float yaw)
-{
-   giroscopio[0] = pitch;
-   giroscopio[1] = roll;
-   giroscopio[2] = yaw;
-}
+    // Set o pitch,roll e o yaw de acordo com as posições dos dados
+    void Dados::setGiroscopio(float pitch, float roll, float yaw)
+    {
+        giroscopio[0] = pitch;
+        giroscopio[1] = roll;
+        giroscopio[2] = yaw;
+    }
 
-// Retorna o valor de pitch
-float Dados::getGiroscopio_pitch(){
-    return giroscopio[0];
-}    
-// Retorna o valor de roll
-float Dados::getGiroscopio_roll(){
-    return giroscopio[1];
-}
-// Retorna o valor de yaw
-float Dados::getGiroscopio_yaw(){
-    return giroscopio[2];
-}
-// Set o altimetro recebido dos dados
-void Dados::setAltimetro(int altimetro)
-{
-    this->altimetro = altimetro;
-}
-// Retorna o valor do altimetro
-int Dados::getAltimetro()
-{
-    return altimetro;
-}
-// Set o pitot recebido dos dados
-void Dados::setPitot(float pitot)
-{
-    this->pitot = pitot;
-}
-// Retorna o pitot
-float Dados::getPitot()
-{
-    return pitot;
-}
-    
-}//namespace SimuladorDeVooSimples
+    // Retorna o valor de pitch
+    float Dados::getGiroscopio_pitch()
+    {
+        return giroscopio[0];
+    }
+    // Retorna o valor de roll
+    float Dados::getGiroscopio_roll()
+    {
+        return giroscopio[1];
+    }
+    // Retorna o valor de yaw
+    float Dados::getGiroscopio_yaw()
+    {
+        return giroscopio[2];
+    }
+    // Set o altimetro recebido dos dados
+    void Dados::setAltimetro(int altimetro)
+    {
+        this->altimetro = altimetro;
+    }
+    // Retorna o valor do altimetro
+    int Dados::getAltimetro()
+    {
+        return altimetro;
+    }
+    // Set o pitot recebido dos dados
+    void Dados::setPitot(float pitot)
+    {
+        this->pitot = pitot;
+    }
+    // Retorna o pitot
+    float Dados::getPitot()
+    {
+        return pitot;
+    }
+
+} //namespace SimuladorDeVooSimples
--- main.cpp
+++ main.cpp
@@ -8,14 +8,17 @@ namespace svs = SimuladorDeVooSimples;
 int main()
 {
     ifstream arquivo;
-    
+
     arquivo.open("dados-decolagem.txt");
-    
-    if(!arquivo.is_open()){
+
+    if (!arquivo.is_open())
+    {
         std::cout << "Arquivo nao foi aberto" << std::endl;
         return 0;
     }
-    
-    
+
+    svs::Aviao pequeno;
+    pequeno.imprimiDados();
+
     return 0;
 }
